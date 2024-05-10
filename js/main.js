9277a0686901d022c6ad64f65e0e79d728405d4d
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-10 01:15:49 -0500

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//create a function that adds two numbers and alerts the sum
+let addTwoNumbers = (num1, num2) => alert (num1 + num2);
+
+addTwoNumbers(1, 3);
+addTwoNumbers(2, 8);
+addTwoNumbers(19, 20);
+
+// create a function that multiplys three numbers and console logs the product
+
+let multiplyThreeNumbers = (n1, n2, n3) => console.log (n1 * n2 * n3);
+
+multiplyThreeNumbers(1, 2, 3);
+multiplyThreeNumbers(3, 5, 6);
+multiplyThreeNumbers(10, 20, 320);
+
+//create a function that divides two numbers and returns the ???
+let dividesTwoNumbers = (d1, d2) => { 
+    let div = d1/d2;
+    return div;
+}
+dividesTwoNumbers(100, 2);
