4e4271f4a6cc4ebd0d4bcf8bed48c1e099d45c43
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-20 15:15:36 -0500

    Add functions with default values.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-//create a function that adds two numbers and alerts the sum
+//1. create a function that adds two numbers and alerts the sum
 let addTwoNumbers = (num1, num2) => alert (num1 + num2);
 
 addTwoNumbers(1, 3);
 addTwoNumbers(2, 8);
 addTwoNumbers(19, 20);
 
-// create a function that multiplys three numbers and console logs the product
+//2. create a function that multiplys three numbers and console logs the product
 
 let multiplyThreeNumbers = (n1, n2, n3) => console.log (n1 * n2 * n3);
 
@@ -13,9 +13,18 @@ multiplyThreeNumbers(1, 2, 3);
 multiplyThreeNumbers(3, 5, 6);
 multiplyThreeNumbers(10, 20, 320);
 
-//create a function that divides two numbers and returns the ???
+//3. create a function that divides two numbers and returns the ???
 let dividesTwoNumbers = (d1, d2) => { 
     let div = d1/d2;
     return div;
 }
 dividesTwoNumbers(100, 2);
+
+//4. // Defining the function with default values
+function sum(num1 = 6, num2 = 1) {
+    return num1 + num2;
+  }
+  
+  // Calling the function
+  console.log(sum(8));
+  // Output 9
